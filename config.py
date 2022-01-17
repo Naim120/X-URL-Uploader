ae1a80a38d5c052b51bc82edba5b634a66a86d7b
sk naim <sknaimuddin234@gmail.com>
2022-01-17 11:43:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ class Config(object):
     # get a token from https://chatbase.com
     CHAT_BASE_TOKEN = os.environ.get("CHAT_BASE_TOKEN", "")
     # get a token from @BotFather
-    TG_BOT_TOKEN = "5009980237:AAFWNJX06bNTo65UCiGXJwzdQ2oy5STolOI"
+    TG_BOT_TOKEN = "5066942273:AAGE9lMpk4YZw-SgdMqHD4PXO-EZMMTsPqA"
     # The Telegram API things
-    APP_ID = 1492128
-    API_HASH = "496a1aab7943406f28e3de49fff16ea2"
+    APP_ID = 1567102
+    API_HASH = "07e9ab27a1d8d479471f81e7848731ad"
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
     AUTH_USERS = "1185346119"
@@ -24,7 +24,7 @@ class Config(object):
     # chunk size that should be used with requests
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     # default thumbnail to be used in the videos
-    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://telegra.ph/file/c62f129a5df7c62f55a6b.jpg")
     # proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
